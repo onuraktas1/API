65458388f412606089a2586007181408f63fed3d
Onur Aktas <onuraktas0640@gmail.com>
2025-02-24 15:01:14 +0300

    dto

---
+++ ApiProject/ApiProject.WebApi/Dtos/ContactDtos/CreateContactDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProject.WebApi.Dtos.ContactDtos
+{
+    public class CreateContactDto
+    {
+        public string MapLocation { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string OpenHours { get; set; }
+    }
+}
