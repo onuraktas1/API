ec1c93ea5369f90d8bdace43de99eb77a3420c0a
Onur Aktas <onuraktas0640@gmail.com>
2025-03-10 19:23:23 +0300

    Dto

---
+++ ApiProject/ApiProject.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using ApiProject.WebApi.Dtos.FeatureDtos;
+using ApiProject.WebApi.Dtos.MessageDtos;
+using ApiProject.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProject.WebApi.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
