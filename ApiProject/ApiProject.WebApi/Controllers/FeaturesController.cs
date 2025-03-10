1ca5f9a2e900ed0fe2056a1553236c5170cab470
Onur Aktas <onuraktas0640@gmail.com>
2025-03-11 01:19:18 +0300

    Create FeaturesController.cs

---
+++ ApiProject/ApiProject.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,62 @@
+﻿using ApiProject.WebApi.Context;
+using ApiProject.WebApi.Dtos.FeatureDtos;
+using ApiProject.WebApi.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ApiContext _context;
+
+        public FeaturesController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult FeauturesList()
+        {
+            var values = _context.Features.ToList();
+            return Ok(_mapper.Map<List<ResultFeatureDto>>(values));
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDto);
+            _context.Features.Add(value);
+            return Ok("Ekleme Başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            _context.Features.Remove(value);
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpGet("GetFeature")]
+        public IActionResult GetFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            return Ok(_mapper.Map<GetByIdFeatureDto>(value));
+        }
+
+        [HttpPut]
+
+        public IActionResult UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(updateFeatureDto);
+            _context.Features.Update(value);
+            return Ok("Güncelleme Başarılı");
+
+        }
+    }
+}
